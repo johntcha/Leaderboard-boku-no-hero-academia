68db82260c5588fb894a54a8553b1749f313858f
John <john.tcha@connected-robotics.com>
2024-06-22 18:04:39 +0900

    mock data + display player cards looping through data

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,18 +1,63 @@
 import PlayerCard from "./PlayerCard";
+import { Character } from "./type";
+
+let mockApiCharacterData: Character[] = [
+  {
+    name: "Izuku Midoriya",
+    imgSrc: "/images/midoriya.jpg",
+    points: 0,
+  },
+  {
+    name: "Katsuki Bakugo",
+    imgSrc: "/images/katsuki.jpg",
+    points: 0,
+  },
+  {
+    name: "Todoroki Shoto",
+    imgSrc: "/images/todoroki.jpg",
+    points: 0,
+  },
+  {
+    name: "Shinso Hitoshi",
+    imgSrc: "/images/shinso.jpg",
+    points: 0,
+  },
+  {
+    name: "Uraraka Ochaco",
+    imgSrc: "/images/uraraka.jpg",
+    points: 0,
+  },
+  {
+    name: "Yaoyorozu Momo",
+    imgSrc: "/images/yaoyorozu.jpg",
+    points: 0,
+  },
+  {
+    name: "Fumikage Tokoyami",
+    imgSrc: "/images/tokoyami.jpg",
+    points: 0,
+  },
+  {
+    name: "Iida tenya",
+    imgSrc: "/images/iida.jpg",
+    points: 0,
+  },
+];
 
 const Leaderboard = () => {
   return (
     <div className="flex flex-col items-center bg-cool-background bg-cover bg-center bg-no-repeat min-h-screen p-8 gap-8">
       <p className="text-6xl text-white">Leaderboard</p>
       <div className="flex flex-col items-center bg-violet-900 w-6/12 max-w-screen-sm rounded p-8 gap-4 shadow-2xl">
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
+        {mockApiCharacterData.map((character, index) => {
+          return (
+            <PlayerCard
+              key={`${index}-${character}`}
+              character={character}
+              rank={index + 1}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/PlayerCard.tsx
+++ src/PlayerCard.tsx
@@ -1,18 +1,22 @@
-const PlayerCard = () => {
+import { Character } from "./type";
+
+interface PlayerCardProps {
+  character: Character;
+  rank: number;
+}
+
+const PlayerCard = ({ character, rank }: PlayerCardProps) => {
+  const { name, imgSrc, points } = character;
   return (
     <div className="bg-violet-600 rounded-md text-2xl w-full shadow-md flex gap-8 px-4 py-2 items-center text-white">
-      <p>1</p>
+      <p>{rank}</p>
       <div className="flex gap-4 items-center">
-        <img
-          src="/images/midoriya.jpg"
-          alt="midoriya"
-          className="h-12 w-12 rounded-full"
-        />
-        <p>name</p>
+        <img src={imgSrc} alt={name} className="h-12 w-12 rounded-full" />
+        <p>{name}</p>
       </div>
       <div className="flex ml-auto gap-2">
-        <p>123</p>
-        <p>points</p>
+        <p>{points}</p>
+        <p>point{points > 1 ? "s" : ""}</p>
       </div>
     </div>
   );
